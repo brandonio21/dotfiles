645d9a5070ceb6cb9f9a1e7bab133810cbf8b42b
Brandon Milton <brandonio21@linux.com>
2015-10-21 22:45:20 -0700

    Made configure script update i3 config

--- configure.sh
+++ configure.sh
@@ -8,3 +8,11 @@ wirelessCard=$(ifconfig -s | cut -d " " -f 1 | egrep "w.+")
 ethernetCard=$(ifconfig -s | cut -d " " -f 1 | egrep "e.+")
 sed "s/WIRELESS_CARD/${wirelessCard}/g" -i i3/.i3/conkyrc
 sed "s/ETHERNET_CARD/${ethernetCard}/g" -i i3/.i3/conkyrc
+
+numMonitors=$(xrandr --current | grep " connected" | wc -l)
+if [ $numMonitors -gt 1 ]; then
+	cat i3/.i3/config.desktop > i3/.i3/config
+else
+	cat i3/.i3/config.laptop > i3/.i3/config
+fi
+
