4c0d16cf88cb31518787db09b1db87410a354968
Brandon Milton <brandonio21@linux.com>
2015-10-21 22:35:24 -0700

    updated config script

--- configure.sh
+++ configure.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Move the template to the actual conkyrc file
+cp i3/.i3/conkyrc.template i3/.i3/conkyrc
+
+# Replace the wireless card for the conky configuration
 wirelessCard=$(ifconfig -s | cut -d " " -f 1 | egrep "w.+")
 ethernetCard=$(ifconfig -s | cut -d " " -f 1 | egrep "e.+")
-cp i3/.i3/conkyrc.template i3/.i3/conkyrc
 sed "s/WIRELESS_CARD/${wirelessCard}/g" -i i3/.i3/conkyrc
 sed "s/ETHERNET_CARD/${ethernetCard}/g" -i i3/.i3/conkyrc
