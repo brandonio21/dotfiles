51c4be3ed2030bff4c9baf34928546a5f201e95c
Brandon Milton <brandonio21@linux.com>
2015-10-22 11:46:05 -0700

    Switched configure script to ip

--- configure.sh
+++ configure.sh
@@ -4,10 +4,13 @@
 cp i3/.i3/conkyrc.template i3/.i3/conkyrc
 
 # Replace the wireless card for the conky configuration
-wirelessCard=$(ifconfig -s | cut -d " " -f 1 | egrep "w.+")
-ethernetCard=$(ifconfig -s | cut -d " " -f 1 | egrep "e.+")
-sed "s/WIRELESS_CARD/${wirelessCard}/g" -i i3/.i3/conkyrc
-sed "s/ETHERNET_CARD/${ethernetCard}/g" -i i3/.i3/conkyrc
+wirelessCard=$(ip link | egrep "[0-9]+: .+: " | cut -d " " -f 2 | egrep "w.+")
+ethernetCard=$(ip link | egrep "[0-9]+: .+: " | cut -d " " -f 2 | egrep "e.+")
+if [[ $ethernetCard == "" ]]; then
+	ethernetCard="enp:"
+fi
+sed "s/WIRELESS_CARD/${wirelessCard%?}/g" -i i3/.i3/conkyrc
+sed "s/ETHERNET_CARD/${ethernetCard%?}/g" -i i3/.i3/conkyrc
 
 numMonitors=$(xrandr --current | grep " connected" | wc -l)
 if [ $numMonitors -gt 1 ]; then
