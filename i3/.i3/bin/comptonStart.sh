fd32cf3c1381a145a109e4253d78867c1895edbb
Brandon Milton <brandon.milton21@gmail.com>
2016-04-15 11:20:49 -0700

    Updated compton start script, added lock script

--- i3/.i3/bin/comptonStart.sh
+++ i3/.i3/bin/comptonStart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sleep 1
-nvidia-settings --load-config-only
+if hash nvidia-settings 2>/dev/null; then
+	nvidia-settings --load-config-only
+fi
 sleep 1
-compton $HOME/.compton.conf -b
+compton --config $HOME/.compton.conf -b
