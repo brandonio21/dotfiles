07e55b08917218a624d9d5ebc16f44cadc7d103c
Brandon Milton <brandon.milton21@gmail.com>
2016-07-03 23:12:08 -0700

    Added dunst and notification for wifi connect and disconnect. Added new layout for systemd files

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sudo cp * /etc/systemd/system
-sudo systemctl daemon-reload
+if [ ! -d ~/.config/systemd/user ]; then
+	mkdir -p ~/.config/systemd/user
+fi
+
+if [ ! -d /etc/systemd/system ]; then
+	sudo mkdir -p /etc/systemd/system
+fi
+
+cp user/* ~/.config/systemd/user
+sudo cp system/* /etc/systemd/system
+
 printf "Installed. Remember to enable all units\n"
